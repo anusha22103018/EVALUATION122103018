7e3b80f0ecb21434a0d20ff6ff4a2fcaeb7a1c1c
anusha22103018 <22103018@MAIL.JIIT.AC.IN>
2024-08-29 10:25:51 +0530

    Add files via upload

---
+++ Downloads/anushaeval.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import nmpy as np
+
+def manage_student_grades(h,w,c):
+if[(h>177) or (w>70) or (c>100)]
+   return("red")
+    elif (h<177) or (w<70) or (c<100)
+    return("orange")
+    if (h=177) or (w=70) or (c=100)
+    return("green")
+
+ 
+data=( {"80", "178", "200"}
+     {"70", "177","100"} )
+
+
+height=int(input("enter your height:"))
+weight=int(input("enter your weight:"))
+name=str(input("enter your name:"))
+sport=str(input("enter your sport interested:"))
+height=int(input("enter your height:"))
+ccalorie=int(input("enter your calorie:"))
+indicator= manage_student_grades(height, weight, calorie)
+
+print(name)
+print(indicator)
+
+if(indicator="red")
+print("Underweight")
+
+elif(indicator="orange")
+print("Needs More Intake")
+
+if(indicator="green")
+print("Sufficient Consumption")
+
+
+
+
+
+# In[7]:
+
+
+import nmpy as np
+
+def manage_student_grades(h,w,c):
+if (h>177) or (w>70) or (c>100)
+   return("red")
+    elif (h<177) or (w<70) or (c<100)
+    return("orange")
+    if (h=177) or (w=70) or (c=100)
+    return("green")
+
+ 
+
+    
+data=( 1:{"80", "178", "200"}
+       2: {"70", "177","100"} )
+
+
+height=int(input("enter your height:"))
+weight=int(input("enter your weight:"))
+name=str(input("enter your name:"))
+sport=str(input("enter your sport interested:"))
+height=int(input("enter your height:"))
+calorie=int(input("enter your calorie:"))
+
+indicator= manage_student_grades(height, weight, calorie)
+
+
+print(indicator)
+
+if(indicator="red")
+print("Underweight")
+
+elif(indicator="orange")
+print("Needs More Intake")
+
+if(indicator="green")
+print("Sufficient Consumption")
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
